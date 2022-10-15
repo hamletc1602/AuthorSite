46739b17fe1d5508f736bc071bc3729f4b3fec24
Adriaan <adriaan@braevitae.com>
2022-10-15 14:20:38 -0600

    More moving files. Added build script.

--- build.sh
+++ build.sh
@@ -2,22 +2,39 @@
 wdir=$(pwd)
 
 # package admin worker lambda
-admin/npm run pack
-mv admin/autosite-admin.zip target
+cd admin
+npm i >/dev/null
+npm run pack >/dev/null
+mv autosite-admin.zip $wdir/target/AutoSite/provision
+cd $wdir
 
 # package provisioner plugin lambda
-provisioner/npm run pack
-mv provisioner/autosite-provisioner.zip target
+cd provisioner
+npm i >/dev/null
+npm run pack >/dev/null
+mv autosite-provisioner.zip $wdir/target/AutoSite/provision
+cd $wdir
 
 # Package edge lambdas
 cd edge
-zip -r target/lambdas.zip *
+zip -qr $wdir/target/AutoSite/provision/lambdas.zip *
+cd $wdir
+
+# package site generator lambda
+cd generators/authorsite
+npm i >/dev/null
+npm run pack >/dev/null
+mv authorsite.zip $wdir/target/AutoSite/builders
+cd $wdir
 
 # Compile UI templates into admin UI source
 handlebars static/admin/templates/desktop/admin.handlebars -f static/adminui/desktop/admin.handlebars.js
-handlebars static/admin/templates/mobile/admin.handlebars -f static/adminui/mobile/admin.handlebars.js
+#handlebars static/admin/templates/mobile/admin.handlebars -f static/adminui/mobile/admin.handlebars.js
 
 # Package admin UI files
 cd static/adminui
-zip -r target/adminui.zip *
+zip -qr $wdir/target/AutoSite/provision/adminui.zip *
 cd $wdir
+
+# Copy CFN provisioning template
+cp AuthorSite.template target/AutoSite
