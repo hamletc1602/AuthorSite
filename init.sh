589953cc6ea225810f45fef873cb1364e6fc86e2
Adriaan <adriaan@braevitae.com>
2022-10-30 17:41:28 -0600

    Add symlinks to share awsUtils between projects.

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create soft links to awsUtilsjs file in admin-worker project
 cd edge
-ln -s ../admin/awsUtils.js awsUtils2.js
-cd ../generators/authorsite
-ln -s ../../../admin/awsUtils.js awsUtils2.js
+ln -s ../admin/awsUtils.js awsUtils.js
+cd ../generators/authorsite/app
+ln -s ../../../admin/awsUtils.js awsUtils.js
