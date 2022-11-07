18812d644b58002f588ca1d46c810f5a8d6dec03
Adriaan <adriaan@braevitae.com>
2022-11-06 22:00:52 -0700

    Fixed cfn template issue

--- build.sh
+++ build.sh
@@ -10,48 +10,48 @@ rm -rf target/AutoSite/lambdas/*
 # Clean DS_Store files
 find . -name '.DS_Store' -type f -delete
 
-# echo package admin worker lambda
-# cd admin
-# rm -rf node_modules
-# rm -f admin-worker.zip
-# npm run install-for-aws >/dev/null
-# zip -qr $wdir/target/AutoSite/lambdas/admin-worker.zip * -x "node_modules/*"
-# cd $wdir
+echo package admin worker lambda
+cd admin
+rm -rf node_modules
+rm -f admin-worker.zip
+npm run install-for-aws >/dev/null
+zip -qr $wdir/target/AutoSite/lambdas/admin-worker.zip * -x "node_modules/*"
+cd $wdir
 
-# echo package provisioner plugin lambda
-# cd provisioner
-# rm -rf node_modules
-# rm -f provisioner.zip
-# npm run install-for-aws >/dev/null
-# zip -qr $wdir/target/AutoSite/lambdas/provisioner.zip * -x "node_modules/*"
-# cd $wdir
+echo package provisioner plugin lambda
+cd provisioner
+rm -rf node_modules
+rm -f provisioner.zip
+npm run install-for-aws >/dev/null
+zip -qr $wdir/target/AutoSite/lambdas/provisioner.zip * -x "node_modules/*"
+cd $wdir
 
-# echo Package edge lambdas
-# cd edge
-# rm -rf node_modules
-# rm -f edge.zip
-# npm run install-for-aws >/dev/null
-# npm run pack
-# mv edge.zip $wdir/target/AutoSite/lambdas/
-# cd $wdir
+echo Package edge lambdas
+cd edge
+rm -rf node_modules
+rm -f edge.zip
+npm run install-for-aws >/dev/null
+npm run pack
+mv edge.zip $wdir/target/AutoSite/lambdas/
+cd $wdir
 
-# echo package site generator lambda
-# cd generators/authorsite
-# rm -rf node_modules
-# rm -f authorsite.zip
-# npm run install-for-aws >/dev/null
-# zip -qr $wdir/target/AutoSite/lambdas/authorsite.zip * -x "node_modules/*"
-# cd $wdir
+echo package site generator lambda
+cd generators/authorsite
+rm -rf node_modules
+rm -f authorsite.zip
+npm run install-for-aws >/dev/null
+zip -qr $wdir/target/AutoSite/lambdas/authorsite.zip * -x "node_modules/*"
+cd $wdir
 
-# echo Compile UI templates into admin UI source
-# handlebars static/admin/templates/desktop/admin.handlebars -f static/adminui/desktop/admin/admin.handlebars-1.0.0.js
-# #handlebars static/admin/templates/mobile/admin.handlebars -f static/adminui/mobile/admin/admin.handlebars-1.0.0.js
+echo Compile UI templates into admin UI source
+handlebars static/admin/templates/desktop/admin.handlebars -f static/adminui/desktop/admin/admin.handlebars-1.0.0.js
+#handlebars static/admin/templates/mobile/admin.handlebars -f static/adminui/mobile/admin/admin.handlebars-1.0.0.js
 
-# echo Package admin UI files
-# rm -f target/AutoSite/provision/adminui.zip
-# cd static/adminui
-# zip -qr $wdir/target/AutoSite/provision/adminui.zip *
-# cd $wdir
+echo Package admin UI files
+rm -f target/AutoSite/provision/adminui.zip
+cd static/adminui
+zip -qr $wdir/target/AutoSite/provision/adminui.zip *
+cd $wdir
 
 echo Build customized CFN provisioning template versions and copy to target
 cd build
