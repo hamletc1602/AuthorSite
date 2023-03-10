e466914c663ea418373787d6bff4c7805ccc7831
Adriaan <adriaan@braevitae.com>
2023-03-10 01:44:47 -0700

    Copy a shared schema definition into each template when building.

--- build-site-config.sh
+++ build-site-config.sh
@@ -8,12 +8,23 @@ echo Copy Site metadata
 cp site-config/metadata.json $wdir/target/AutoSite/site-config/
 
 echo Package Default site-config
+
 echo Author
-cd site-config/author
+root=/tmp/authorsite/template/author
+mkdir -p $root
+cp -R site-config/author/ $root
+cp site-config/schema/editors.yaml $root/config
+cp -R site-config/schema/schema $root/config/conf/schema
+cd $root
 zip -qr $wdir/target/AutoSite/site-config/author.zip *
 cd $wdir
 
 echo Artist
-cd site-config/artist
-zip -qr $wdir/target/AutoSite/site-config/artist.zip *
+root=/tmp/authorsite/template/artist
+mkdir -p $root
+cp -R site-config/artist/ $root
+cp site-config/schema/editors.yaml $root/config
+cp -R site-config/schema/schema/ $root/config/conf/schema
+cd $root
+zip -qr $wdir/target/AutoSite/site-config/author.zip *
 cd $wdir
