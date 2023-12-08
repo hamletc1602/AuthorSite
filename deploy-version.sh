a4179c049a2ec22625f7b56edf205af81375c564
Adriaan <adriaan@braevitae.com>
2023-12-08 09:27:51 -0700

    Complicate version param in the short term to remain back-compat with unversioned template dir

--- deploy-version.sh
+++ deploy-version.sh
@@ -3,6 +3,8 @@ version=$1
 
 wdir=$(pwd)
 
+# NOTE: It's expected that the version parameter _include_ the leading dash (-) for compatibility
+#       with the current un-versioned dir. Once the main dir is a versioned one, this can be corrected.
 echo Build customized CFN provisioning template versions and copy to target
 cd build
 npm run build -- $version
@@ -14,7 +16,7 @@ cd $wdir
 echo "Do you really want to re-deploy and overwrite version $version of the AutoSite install?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) echo Sync target dir with public bucket; aws s3 sync target/AutoSite s3://braevitae-pub/AutoSite/$version/; break;;
+        Yes ) echo Sync target dir with public bucket; aws s3 sync target/AutoSite s3://braevitae-pub/AutoSite$version/; break;;
         No ) exit;;
     esac
 done
