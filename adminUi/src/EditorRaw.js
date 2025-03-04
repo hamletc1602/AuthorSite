c62665f42583badd22699377755e89e90094a9cf
Adriaan <adriaan@braevitae.com>
2025-03-03 23:58:08 -0700

    no message

--- adminUi/src/EditorRaw.js
+++ adminUi/src/EditorRaw.js
@@ -24,6 +24,40 @@ export default function EditorRaw({id, editor, configs, contentToPut, locked}) {
   // Possibly this componentn does not mix well with Chakra UI?  Searhing suggestrs this may be a MaterialUI based component.
   //   How to properly make theme available??
 
+  // Possibly use the CodeMirror editor directly, or as a wrapper, as in this example:
+  // https://glenn-viroux.medium.com/how-to-create-a-code-editor-in-react-with-chakraui-9d27ed24efcb
+
+  // import React, {useState} from 'react';
+  // import {Center, ChakraProvider, Divider, Heading, VStack} from "@chakra-ui/react";
+  // import {githubLight} from '@uiw/codemirror-theme-github';
+  // import {python} from "@codemirror/lang-python";
+  // import CodeMirror from '@uiw/react-codemirror';
+
+
+  // function App() {
+  //     const [text, setText] = useState("print(\\"Hello world!\\")");
+  //     return (
+  //         <ChakraProvider>
+  //             <Center h={"100vh"}>
+  //                 <VStack boxShadow={'md'} p={4} borderStyle={"solid"} borderWidth={1} rounded={"lg"}>
+  //                     <Heading>Code Editor</Heading>
+  //                     <Divider/>
+  //                     <CodeMirror
+  //                         value={text}
+  //                         onChange={(newValue) => setText(newValue)}
+  //                         theme={githubLight}
+  //                         extensions={[python()]}
+  //                         basicSetup={{autocompletion: true}}
+  //                         minWidth={'500px'}
+  //                         minHeight={'500px'}
+  //                     />
+  //                 </VStack>
+  //             </Center>
+  //         </ChakraProvider>
+  //     );
+  // }
+  // export default App;
+
 
   return
     <YamlEditor
